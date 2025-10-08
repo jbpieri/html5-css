a8eadc553fbe5bd1b18e846dc0aabe62e79eb5d4
João Pieri <jpieri@icloud.com>
2025-10-07 22:11:21 -0300

    c

---
+++ python/programa_001/Arquivos do Módulo 03/arquivo01.py
@@ -0,0 +1,34 @@
+#   Importarção de bibliotecas ou módulos.
+
+# Ex: Determinante de matrizes.
+
+#   Importar uma biblioteca completa.
+import math
+
+#   Importar uma biblioteca e atribuir um apelido para ela.
+import numpy as np
+
+#   chamar uma função do módulo ou biblioteca
+
+arrayNump01 = np.array([1, 2, 3, 4])
+
+print(arrayNump01)
+
+#   Importar somente algumas funções das bibliotecas
+
+from math import sin
+
+#   Exemplos de bibliotecas
+
+import math
+import numpy
+import pandas
+import fractions
+import sympy
+import pylab
+import matplotlib
+import cmath
+import tabulate
+
+#   Instalação de bibliotecas
+#   Vai digitar nno terminal: pip install xxxxxxxx
