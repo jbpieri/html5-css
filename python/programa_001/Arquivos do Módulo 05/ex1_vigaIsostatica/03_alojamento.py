789347a92527f783e85ff028dba396df49bcc34a
João Pieri <jpieri@icloud.com>
2025-10-24 11:30:21 -0300

    A

--- python/programa_001/Arquivos do Módulo 05/ex1_vigaIsostatica/03_alojamento.py
+++ python/programa_001/Arquivos do Módulo 05/ex1_vigaIsostatica/03_alojamento.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-
-
 def alojVigas(bw, cob, diamEstribo, diamAgreg, bitola):
     
     # bw (cm)
@@ -11,14 +9,32 @@ def alojVigas(bw, cob, diamEstribo, diamAgreg, bitola):
     # bitola (mm)
     
     dispAloj = bw - (2 * cob) - (2 * diamEstribo / 10)
-    
-    
     quantBarras = np.linspace(2, 100, 99)
     
     ah = (dispAloj - (quantBarras * bitola / 10)) / (quantBarras - 1)
     
+    condicoes = [2.0, bitola / 10, 1.2 * diamAgreg / 10]
+    valorLimite = max(condicoes)
     
-    return ah
+    ahSelect = ah[ah >= valorLimite]
+    
+    # Caractere unicode u03C6 pesquisar
+    
+    for i in range(len(ahSelect)):
+        print(f'{i + 2} \u03C6 {bitola} -> espaçamento = {ahSelect[i]} cm')
+    
+    return ahSelect
 
-print(alojVigas(20, 3, 5.0, 19, 10.0))
+def opcoesAloj(bw, cob, diamEstribo, diamAgreg):
+    
+    listaBitolas = [5.0, 6.3, 8.0, 10.0, 12.5, 16.0, 20.0, 22.0, 25.0, 32.0, 40.0]
+    
+    for bitola in listaBitolas:
+        
+        alojVigas(bw, cob, diamEstribo, diamAgreg, bitola)
+    
+    return
 
+# EXECUÇÕES
+
+opcoesAloj(20, 3, 5.0, 19)
