54ab6e21bdd72e533694f45adc59f43adf51ac6b
João Pieri <jpieri@icloud.com>
2025-10-28 13:05:30 -0300

    e

---
+++ python/programa_001/Arquivos do  Módulo 06/cargaAdmEstacas/tabelasPaametros.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def paramAokiTab():
+    coefKeAlfa = [
+        [1, 1000, 0.014],
+        [12, 800, 0.020],
+        [123, 700, 0.024],
+        [13, 600, 0.030],
+        [132, 500, 0.028],
+        [2, 400, 0.030],
+        [21, 550, 0.022],
+        [213, 450, 0.028],
+        [23, 230, 0.034],
+        [231, 450, 0.030],
+        [3, 200, 0.060],
+        [31, 350, 0.024],
+        [312, 300, 0.028],
+        [32, 220, 0.040],
+        [321, 330, 0.030]
+        
+    ]
+    
+    dfParamAoki = pd.DataFrame(coefKeAlfa, columns=['Solo', 'K (kPa)', 'Alfa'])
+    
+    
+    return dfParamAoki
+
+
+def fatorCorrAoki():
+    
+    corrAoki = [
+        ['Escavada', 3.0, 6],
+        ['Raiz', 2.0, 4],
+        ['HCM', 2.0, 4]
+    ]       
+    
+    dfCorrecaoAoki = pd.DataFrame(corrAoki, columns=['Tipo de estaca', 'F1', 'F2'])
+      
+    return dfCorrecaoAoki
+
+print(paramAokiTab())
+
+print(fatorCorrAoki())
---
+++ python/programa_001/Arquivos do  Módulo 06/estacas/AulaPandasParte04.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import openpyxl
+# pip install openpyxl para trabalhar com excel
+
+#df = pd.read_csv('arquivoExcelTeste1.csv')
+
+
+df = pd.read_excel('excel.xlsx'),
+
+print(df)
